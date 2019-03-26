f1874e8c1a020facf8c4263f625edfa231d1d3bf
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2019-03-26 21:38:36 +0000

    deleteClient

--- src/agencyclient.cpp
+++ src/agencyclient.cpp
@@ -16,6 +16,14 @@ void Agency::loadClients(std::string fpath){
     }
 }
 
+void Agency::addClient(){
+    Client c;
+    if(c.userClient()){
+        vclient.insert(c);
+        std::cout << "Cliente adicionado" << std::endl;
+    }
+}
+
 std::pair<unsigned, bool> Agency::seeClient() const{
     Client::print(vclient.begin(), vclient.end(), "table") << std::endl;
     int i;
@@ -30,10 +38,20 @@ std::pair<unsigned, bool> Agency::seeClient() const{
     return std::make_pair(i, true);
 }
 
-void Agency::addClient(){
-    Client c;
-    if(c.userClient()){
-        vclient.insert(c);
-        std::cout << "Cliente adicionado" << std::endl;
+void Agency::deleteClient(){
+    auto p = seeClient();
+    if(!p.second) return;
+    int i = p.first;
+    std::string b; std::cout << std::endl;
+    while(true){
+        if(!vin("Confirme que pretende eliminar o cliente #"+std::to_string(i)+" [y/n]: ", b)) return;
+        b = lower_case(b);
+        if(b == "y" || b == "n") break;
+        std::cout << "Apenas [y] (sim) ou [n] (não) são possibilidades válidas" << std::endl;
+    }
+    if(b == "y"){
+        auto it = vclient.begin(); std::advance(it, i);
+        vclient.erase(it);
+        std::cout << "Cliente eliminado" << std::endl;
     }
 }
