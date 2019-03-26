9b0cd7a1bad2f42788f290c3bdd23d3b8f2af485
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2019-03-26 11:07:39 +0000

    a

--- src/agencyclient.cpp
+++ src/agencyclient.cpp
@@ -1,5 +1,8 @@
 #include "agency.h"
 
+#include <stdexcept>
+
 void Agency::loadClients(std::string s){
-    
+    std::ifstream is(s);
+    if(!is.is_open()) throw std::invalid_argument("could not open client file");
 }
