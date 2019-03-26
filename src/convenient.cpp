afa7b47095ccb417c26387856da5f3adcba94039
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2019-03-26 10:07:29 +0000

    update

--- src/convenient.cpp
+++ src/convenient.cpp
@@ -1,1 +1,32 @@
-======
+#include "convenient.h"
+
+std::string trim(const std::string& s){
+    return std::string(s.begin()+s.find_first_not_of(' '),
+                       s.begin()+s.find_last_not_of(' ')+1);
+}
+
+std::vector<std::string> parse(std::string s, char delim){
+    std::vector<std::string> ret;
+    int i;
+    while(true){
+        i = s.find(delim);
+        if(i == s.npos) break;
+        ret.push_back(trim(std::string(s.begin(), s.begin()+i)));
+        s = std::string(s.begin()+i+1, s.end());
+    }
+    ret.push_back(trim(s));
+    return ret;
+}
+
+std::string setwidth(std::string s, int sz){
+    if(s.size() < sz)
+        s += std::string(sz-s.size(), ' ');
+    if(s.size() > sz)
+        s = std::string(s.begin(), s.end()-3) + "...";
+    return s;
+}
+
+std::string lower_case(std::string s){
+    for(auto& c:s) c = tolower(c);
+    return s;
+}
