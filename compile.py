ab15c3ae23290ff609926a95aefed85893c71b8c
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2019-03-26 19:07:13 +0000

    organize stuff

--- compile.py
+++ compile.py
@@ -2,10 +2,12 @@ import os
 import platform
 
 if platform.system() == "Linux":
-    print("Create ./linux/obj/, remove all content")
-    os.mkdir("linux/obj")
-    for f in os.listdir("./linux/obj"): os.remove(f)
+    print("Create ./build/linux/obj/, remove all content")
+    os.system("mkdir -p build/linux/obj")
+    for f in os.listdir("build/linux/obj"):
+        os.remove(f)
 
+"""
 elif platform.system() == "Windows":
     print("Create ./msw/obj/, remove all content")
     if not os.path.exists("msw\\obj"):
@@ -18,3 +20,4 @@ elif platform.system() == "Windows":
         if f[-4:] != ".cpp": continue
         print("g++ -Wall -std==c++11 -c src\\%s -o msw\\obj\\%s -I.\\include\\"%(f, f))
         os.system("g++ -Wall -std==c++11 -c src\\%s -o msw\\obj\\%s -I.\\include\\"%(f, f))
+"""
